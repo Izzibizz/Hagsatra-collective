22b92c62a1eaeb9bd772d91daa6126d4b05c861a
izzibizz <contact@izabellind.com>
2025-12-18 15:48:20 +0100

    about

--- client/src/pages/AboutPage.tsx
+++ client/src/pages/AboutPage.tsx
@@ -1,8 +1,30 @@
 import { sanity } from "../library/SanityClient";
 import { useEffect, useState } from "react";
 
+type AboutData = {
+  H2: string;
+  IntroText: string;
+  history: string;
+  activities: {
+    h3: string;
+    body: string;
+    images: {
+      url: string;
+      caption?: string;
+    }[];
+  };
+  exhibitions: {
+    h3: string;
+    body: string;
+    images: {
+      url: string;
+      caption?: string;
+    }[];
+  };
+};
+
 export const AboutPage: React.FC = () => {
-  const [aboutData, setAboutData] = useState();
+  const [aboutData, setAboutData] = useState<AboutData | null>(null);
 
   const query = `
 *[_type == "page" && slug.current == "about"][0]{
@@ -35,10 +57,16 @@ export const AboutPage: React.FC = () => {
   useEffect(() => {
     sanity.fetch(query).then((data) => {
       console.log(data);
-      setAboutData(data);
+      setAboutData(data.sections);
     });
   }, []);
 
-  console.log(aboutData)
-  return <section>Om oss</section>;
+  console.log(aboutData);
+  return (
+    <section className="w-11/12 laptop:w-9/12 mx-auto mt-10">
+      <h2 className="font-hagsatra text-4xl ">{aboutData?.H2}</h2>
+      <p>{aboutData?.IntroText}</p>
+      <img src={aboutData?.exhibitions.images[0].url} />
+    </section>
+  );
 };
