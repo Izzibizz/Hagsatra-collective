edcc5f5fd22ad96f306c8acb7ec0af2ef9ef0539
izzibizz <contact@izabellind.com>
2026-01-19 14:47:05 +0100

    favi

--- client/src/components/HomeHelmet.tsx
+++ client/src/components/HomeHelmet.tsx
@@ -4,6 +4,6 @@ export const HomeHelmet: React.FC = () => (
   <Helmet>
     <title>Hagsätra Collective – Konstnärsateljéer, utställningar & kreativa workshops</title>
     <meta name="description" content="Vi är förening med lokal i Hagsätra, som erbjuder konstnärer i Stockholm att bli medlem och hyra ateljé. Vi orgaiserar utställningar och öppna kreativa workshops för barn och vuxna." />
-    <link rel="icon" type="image/png" sizes="180x180" href="/HC-Logotyp-monogram-80.png" />
+    <link rel="icon" type="image/png" sizes="192x192" href="/HC-Logotyp-monogram-192.png" />
   </Helmet>
 );
