6fdd73b1f1be0faa4386c52ceea254c67fd01159
izzibizz <contact@izabellind.com>
2026-01-19 21:59:15 +0100

    helmet

--- client/src/components/HomeHelmet.tsx
+++ client/src/components/HomeHelmet.tsx
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet";
 export const HomeHelmet: React.FC = () => (
   <Helmet>
     <title>Hagsätra Collective – Konstnärsateljéer, utställningar & kreativa workshops</title>
-    <meta name="description" content="Vi är förening med lokal i Hagsätra, som erbjuder konstnärer i Stockholm att bli medlem och hyra ateljé. Vi orgaiserar utställningar och öppna kreativa workshops för barn och vuxna." />
+    <meta name="description" content="HC är en ateljéförening med lokal i Hagsätra, som erbjuder konstnärer i Stockholm att bli medlem och hyra ateljé. Vi orgaiserar utställningar och öppna kreativa workshops för barn och vuxna." />
     <link rel="icon" type="image/png" sizes="192x192" href="/HC-Logotyp-monogram-192.png" />
   </Helmet>
 );
